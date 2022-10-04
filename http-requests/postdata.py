4396c8d49f63a65c0a489e7aaa22c71a8c7ef7c4
Santosh Thomas <santhom@microsoft.com>
2022-10-04 15:17:40 -0700

    Changes as of 'Tue Oct  4 15:17:40 PDT 2022'

--- http-requests/postdata.py
+++ http-requests/postdata.py
@@ -11,20 +11,21 @@ if __name__ == "__main__":
     postdata = dict()
     queries = list()
     postdata['database']  = 'dev'
-    postdata['container'] = 'travel'
+    postdata['container'] = 'volcano'
     postdata['queries']   = queries
     
     q1 = dict()
-    q1['sql'] = "select * from c where c.pk = 'GUM:MAJ' offset 0 limit 3"
-    q1['count'] = 4 
+    #q1['sql'] = "select * from c where c.pk = 'GUM:MAJ' offset 0 limit 3"
+    q1['sql'] = "select * from c where c.Region = 'Kamchatka'"
+    q1['count'] = 6
     q1['verbose'] = 'true'
 
-    q2 = dict()
-    q2['sql'] = "select * from c where c.pk = 'CLT:MBJ'"
-    q2['count'] = 2 
-    q2['verbose'] = 'false'
+    #q2 = dict()
+    #q2['sql'] = "select * from c where c.pk = 'CLT:MBJ'"
+    #q2['count'] = 2 
+    #q2['verbose'] = 'false'
 
     queries.append(q1)
-    queries.append(q2)
+    #queries.append(q2)
 
     print(json.dumps(postdata, sort_keys=False, indent=2))
