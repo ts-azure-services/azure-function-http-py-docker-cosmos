02af97eba78b86d4a71645e333b9479441447e19
Santosh Thomas <santhom@microsoft.com>
2022-10-04 12:59:41 -0700

    Changes as of 'Tue Oct  4 12:59:41 PDT 2022'

--- http-requests/http_client.py
+++ http-requests/http_client.py
@@ -2,7 +2,7 @@
   python http_client.py <function_url> <auth_secret> <body_json_file>
   python http_client.py https://cjoakimfunctions.azurewebsites.net/api/HttpCosmos ppqXXXXX postdata/body1.json
 """
-import sys
+import os
 import json
 import requests
 from dotenv import load_dotenv
@@ -11,9 +11,6 @@ def load_variables():
     """Load authentication details"""
     env_var=load_dotenv('./variables.env')
     auth_dict = {
-            "resource_group":os.environ['RESOURCE_GROUP'],
-            "storage_acct_name":os.environ['STORAGE_ACCT_NAME'],
-            "storage_conn_string":os.environ['STORAGE_CONN_STRING'],
             "function_app_name":os.environ['FUNCTIONAPPNAME'],
             "function_app_secret":os.environ['FUNCTIONAPP_SECRET'],
             "function_url":os.environ['FUNCTIONAPP_URL'],
@@ -25,9 +22,9 @@ def read_body_json_file(body_json_file):
         return f.read()
 
 if __name__ == "__main__":
-    body_json_file = sys.argv[3]
     auth_var = load_variables()
-    function_url, auth_secret, body_json_file = auth_dict['function_url'], auth_dict['function_app_secret'], 
+    body_json_file = './postdata/body1.json'
+    function_url, auth_secret = auth_var['function_url'], auth_var['function_app_secret'], 
 
     body = json.loads(read_body_json_file(body_json_file))
 
@@ -48,6 +45,9 @@ if __name__ == "__main__":
     response = requests.post(function_url, headers=headers, json=body)
     print('response: {}'.format(response))
 
-    if response.status_code == 200:
+    try:
+        #if response.status_code == 200:
         resp_obj = json.loads(response.text)
         print(json.dumps(resp_obj, sort_keys=False, indent=2))
+    except Exception as e:
+        print(e)
